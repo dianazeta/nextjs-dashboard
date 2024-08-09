5a7fcf655d703a7ce10f73a45296cadb7e8d040e
Diana Zeta <dianazdenis@gmail.com>
2024-08-09 09:53:26 +0800

    Pagination

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,16 +1,14 @@
-import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
-import { fetchFilteredInvoices } from '@/app/lib/data';
+import Image from "next/image";
+import { UpdateInvoice, DeleteInvoice } from "@/app/ui/invoices/buttons";
+import InvoiceStatus from "@/app/ui/invoices/status";
+import { formatDateToLocal, formatCurrency } from "@/app/lib/utils";
+import { fetchFilteredInvoices } from "@/app/lib/data";
 
-export default async function InvoicesTable({
-  query,
-  currentPage,
-}: {
-  query: string;
-  currentPage: number;
-}) {
+interface Props {
+  query: string | "";
+  currentPage: number | 1;
+}
+export default async function InvoicesTable({ query, currentPage }: Props) {
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
